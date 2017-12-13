355f36d70d4d011c7003c3365f84f27e64ca3dcf
Nick ten Veen <nveen0930@gmail.com>
2017-12-13 08:51:02 +0100

    added test case for collecting errors with play-json

--- play-json/src/test/scala/CmapReadsSpec.scala
+++ play-json/src/test/scala/CmapReadsSpec.scala
@@ -1,14 +1,13 @@
 import com.besuikerd.cmap.integration.playjson.CmapReads
 import org.scalatest.{EitherValues, FlatSpec, MustMatchers}
-import play.api.libs.json.{Format, JsPath, Json}
+import play.api.libs.json._
 
 class CmapReadsSpec extends FlatSpec with MustMatchers with EitherValues {
   implicit val addressFormat: Format[Address] = Json.format[Address]
   implicit val personFormat: Format[Person]   = Json.format[Person]
+  import CmapReads._
 
   "CmapReads" should "wrap Json Reads" in {
-    import CmapReads._
-
     val person = Person("John", "Doe", 42, Address("Baker Street", 123))
 
     val personJson = personFormat.writes(person)
@@ -29,6 +28,35 @@ class CmapReadsSpec extends FlatSpec with MustMatchers with EitherValues {
 
     val result = cmapPerson.runCmap(personJson)
     result mustBe Right(person)
+  }
+
+  it should "collect multiple validation errors" in {
+
+    val json = JsObject(
+      Map(
+        "key1" -> JsString("val1"),
+        "key2" -> JsString("val2")
+      ))
+
+    val path1 = JsPath \ "wrongkey1"
+    val path2 = JsPath \ "wrongkey2"
+
+    val cmap = CmapReads.apply[String, String](
+      path1,
+      path2
+    )
+
+    val result = cmap.runCmap(json)
+
+    result mustBe 'left
+    val Left(errors) = result
+    errors.size mustBe 2
+
+    errors(0)._1 mustBe path1
+    errors(0)._2.head.message mustBe "error.path.missing"
+
+    errors(1)._1 mustBe path2
+    errors(1)._2.head.message mustBe "error.path.missing"
   }
 
 }
